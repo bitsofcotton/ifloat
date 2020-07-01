9f1a7d8ebb594f4c0b15149a48b1ec310e1998df
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-07-01 16:19:57 +0900

    operator T and normalize fix.

--- ifloat.cc
+++ ifloat.cc
@@ -13,54 +13,45 @@ typedef DUInt<di3_t, 512> di4_t;
 typedef DUInt<di4_t, 1024> di5_t;
 typedef DUInt<di5_t, 2048> di6_t;
 typedef DUInt<di6_t, 4096> di7_t;
-//typedef SimpleFloat<uint64_t, DUInt<uint64_t, 64>, 64, short> f_t;
-//typedef SimpleFloat<uint32_t, uint64_t, 32, short> f_t;
-typedef Signed<DUInt<uint8_t, 8>, 16> ii_t;
 typedef Signed<DUInt<uint64_t, 64>, 128> i128_t;
+typedef SimpleFloat<uint32_t, uint64_t, 32, i128_t> fshort_t;
 typedef SimpleFloat<di6_t, di7_t, 4096, i128_t> f_t;
-
 typedef f_t T;
 typedef Complex<T> U;
 
 int main() {
-/*
-  ii_t ij(1);
-  std::cout << ii_t(ij << 15) << std::endl;
-  f_t  ff(2);
-  i128_t ii(1);
-  ii <<= 68;
-  for(int i = 1; i < 64; i ++) {
-    std::cout << pow(ff, pow(ff, f_t(i))) << std::endl;
-  //  f  *= di_t(i);
-  //  ff *= f_t(i);
-  //  std::cout << i << " : " << f << " == " << ff << ", 2 == " << (((di_t(1) << 63) / (di_t(1) << i)) >> (63 - i - 1)) << " 0 != " << floor(f_t(1) << short(i)) << std::endl;
+  for(int i = 1; i < 256; i ++)
+    try {
+      i128_t   f(1);
+      fshort_t ff(1);
+      std::cout << i << " : 1 == " << ((f  << i) >> i) << std::endl;
+      std::cout << i << " : 1 == " << ((ff << (i128_t(1) << i)) >> (i128_t(1) << i)) << std::endl;
+    } catch(const char* e) {
+      std::cout << e << std::endl;
+    }
+  try {
+    f_t test, test2;
+    std::cin >> test;
+    std::cin >> test2;
+    std::cout << test << "+" << test2 << "="<< test + test2 << std::endl;
+    std::cout << test << "-" << test2 << "="<< test - test2 << std::endl;
+    std::cout << test << "*" << test2 << "="<< test * test2 << std::endl;
+    std::cout << test << "/" << test2 << "="<< test / test2 << std::endl;
+    std::cout << "exp(test)      : " << exp(test) << std::endl;
+    std::cout << "log(exp(test)) : " << log(exp(test)) << std::endl;
+    std::cout << "log(test)      : " << log(test) << std::endl;
+    std::cout << "exp(log(test)) : " << exp(log(test)) << std::endl;
+    test = f_t(1) / test;
+    std::cout << "exp(1/test)       : " << exp(test) << std::endl;
+    std::cout << "log(exp(1/test))  : " << log(exp(test)) << std::endl;
+    std::cout << "log(1/test)       : " << log(test) << std::endl;
+    std::cout << "exp(log(1/test))  : " << exp(log(test)) << std::endl;
+    std::cout << "atan(1/test)      : " << test.atan() << std::endl;
+    std::cout << "tan(atan(1/test)) : " << tan(test.atan()) << std::endl;
+    std::cout << "atan(tan(1/test)) : " << (tan(test)).atan() << std::endl;
+  } catch (const char* e) {
+    std::cout << e << std::endl;
   }
-*/
-  f_t test, test2;
- //try{
-  std::cin >> test;
-  std::cin >> test2;
-  std::cout << test << "+" << test2 << "= "<< test + test2 << std::endl;
-  std::cout << test << "-" << test2 << "= "<< test - test2 << std::endl;
-  std::cout << test << "*" << test2 << "= "<< test * test2 << std::endl;
-  std::cout << test << "/" << test2 << "= "<< test / test2 << std::endl;
-  std::cout << "exp(test)      : " << exp(test) << std::endl;
-  std::cout << "log(exp(test)) : " << log(exp(test)) << std::endl;
-  std::cout << "log(test)      : " << log(test) << std::endl;
-  std::cout << "exp(log(test)) : " << exp(log(test)) << std::endl;
-  test = f_t(1) / test;
-  std::cout << "exp(1/test)       : " << exp(test) << std::endl;
-  std::cout << "log(exp(1/test))  : " << log(exp(test)) << std::endl;
-  std::cout << "log(1/test)       : " << log(test) << std::endl;
-  std::cout << "exp(log(1/test))  : " << exp(log(test)) << std::endl;
-  std::cout << "atan(1/test)      : " << test.atan() << std::endl;
-  std::cout << "tan(atan(1/test)) : " << tan(test.atan()) << std::endl;
-  std::cout << "atan(tan(1/test)) : " << (tan(test)).atan() << std::endl;
-/*
- } catch (const char* e) {
-  std::cout << e << std::endl;
- }
-*/
   return 0;
 }
 
